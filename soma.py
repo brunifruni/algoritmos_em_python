ab1e483afd06c067d9b1825e873d9386bd8b7ea6
Bruna <brukifranzon@gmail.com>
2025-05-14 08:51:20 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,5 @@
+n1 = int (input('Digite o primeiro número: '))
+n2 = int (input('Digite o segundo número: '))
+s1 = n1 + n2
+print (f'A soma dos dois números é:', s1)
+print1(f'O resultado da soma de {n1} com {n2} é {n1 + n2}.')
