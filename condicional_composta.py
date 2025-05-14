18b033f202a3cdf658cf048523e7194449354e71
Bruna <brukifranzon@gmail.com>
2025-05-14 08:53:17 -0300

    Create condicional_composta.py

---
+++ condicional_composta.py
@@ -0,0 +1,11 @@
+# ler 2 valores e inteiros e comparar ambos
+#SEMPRE OPTAR POR USAR CONDICIONAIS COMPOSTAS POIS ELAS MELHORAM O DESEPENHO DO  ALGORITMO
+# faz 1 calculo, melhora o desempenho e a velocidade do alg
+
+x = int (input('Digite um valor inteiro: '))
+y= int (input('Digite um segundo valor inteiro: '))
+
+if (x > y):
+    print('O primeiro valor é maior que o segundo.')
+else:
+    print('O segundo valor é maior que o primeiro.')
