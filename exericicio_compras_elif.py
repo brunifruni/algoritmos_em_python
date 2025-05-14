dc7e8305b568d83de72790f0d98f1ac3e6d6dac2
Bruna <brukifranzon@gmail.com>
2025-05-14 08:59:03 -0300

    Create exericicio_compras_elif.py

---
+++ exericicio_compras_elif.py
@@ -0,0 +1,19 @@
+print ('Escolha o que deseja comprar: ')
+print ('1 - Maçã')
+print ('2 - Laranja')
+print ('3 - Banana')
+
+produto = int (input('Digite a opção desejada: '))
+qtd = int(input('Quantas unidades?'))
+
+if (produto == 1):
+  pagar = qtd * 2.3 #2.30 é o valor de 1 maça
+  print(f'Você comprou {qtd} maçãs. O total a pagar é de R$ {pagar}')
+elif (produto == 2): #elif = else + if
+    pagar = qtd * 3.6 #preço da laranja
+    print(f'Você comprou {qtd} laranjas. O total a pagar é de R$ {pagar}')
+elif (produto == 3):
+      pagar = qtd * 1.85
+      print(f'Você comprou {qtd} bananas. O total a pagar é de R$ {pagar}')
+else: #opcional o else
+        print('Produto inexistente')
