2ceefc3728d0300d249bb1df1676f7c521974335
Bruna <brukifranzon@gmail.com>
2025-05-14 08:52:27 -0300

    Create condicional_simples.py

---
+++ condicional_simples.py
@@ -0,0 +1,6 @@
+# ler 2 valores e inteiros e comparar ambos
+x = int (input('Digite um valor inteiro: '))
+y= int (input('Digite um segundo valor inteiro: '))
+
+if (x > y):
+    print('O primeiro valor é maior que o segundo')
