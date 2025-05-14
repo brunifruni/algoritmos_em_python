2bfc0c772e66ab2c571234b7b7d0d7ab37d9f880
Bruna <brukifranzon@gmail.com>
2025-05-14 12:51:35 -0300

    Create estruturas_repeticao_while.py

---
+++ estruturas_repeticao_while.py
@@ -0,0 +1,18 @@
+#while  - contagem de 1 a 5
+x = 1
+while (x <= 5):
+  print(x)
+  x = x + 1
+
+#contagem 1 a 99
+x = 0
+while (x <= 99):
+  print(x)
+  x = x + 1
+
+#contagem de 1 a 99
+x = 0
+while (x < 100):
+  print(x)
+  x = x + 1
+  
